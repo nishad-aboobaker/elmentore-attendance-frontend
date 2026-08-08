c321cf6fe4e57818415c14c45e0c9636b300bce9
nishad pp <nishuanshad@gmail.com>
2026-08-08 12:17:59 +0530

    Fix middleware imports in fund routes

--- backend/src/routes/fund.js
+++ backend/src/routes/fund.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const fundController = require('../controllers/fundController');
-const { protect, checkRole } = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
+const { checkRole } = require('../middlewares/role');
 
-router.use(protect);
+router.use(auth);
 
 router.get('/', fundController.getAllTransactions);
 router.post('/', checkRole('admin'), fundController.createTransaction);
